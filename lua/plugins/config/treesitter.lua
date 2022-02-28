f3b91cc13a01bf1e3029e1fe9f588f5311ca1a43
Moschcau, Kai <kai.moschcau@sabre.com>
2022-02-28 11:22:46 +0100

    Update custom tree sitter captures

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -2,22 +2,6 @@ require 'nvim-treesitter.configs'.setup {
   ensure_installed = 'maintained',
   highlight = {
     enable = true,
-    custom_captures = {
-      ['accessor.keyword'] = 'TSAccessorKeyword',
-      ['accessor.name'] = 'TSAccessorName',
-      ['class.keyword'] = 'TSClassKeyword',
-      ['class.name'] = 'TSClassName',
-      ['enum.keyword'] = 'TSEnumKeyword',
-      ['enum.name'] = 'TSEnumName',
-      ['function.keyword'] = 'TSFunctionKeyword',
-      ['function.name'] = 'TSFunctionName',
-      ['generic.special'] = 'TSGenericSpecial',
-      ['interface.keyword'] = 'TSInterfaceKeyword',
-      ['interface.name'] = 'TSInterfaceName',
-      ['local.name'] = 'TSLocalName',
-      ['namespace.keyword'] = 'TSNamespaceKeyword',
-      ['namespace.name'] = 'TSNamespaceName'
-    },
     additional_vim_regex_highlighting = false
   },
   indent = {
@@ -29,6 +13,23 @@ require 'nvim-treesitter.configs'.setup {
   query_linter = {
     enable = true
   }
+}
+
+require 'nvim-treesitter.highlight'.set_custom_captures {
+  ['accessor.keyword'] = 'TSAccessorKeyword',
+  ['accessor.name'] = 'TSAccessorName',
+  ['class.keyword'] = 'TSClassKeyword',
+  ['class.name'] = 'TSClassName',
+  ['enum.keyword'] = 'TSEnumKeyword',
+  ['enum.name'] = 'TSEnumName',
+  ['function.keyword'] = 'TSFunctionKeyword',
+  ['function.name'] = 'TSFunctionName',
+  ['generic.special'] = 'TSGenericSpecial',
+  ['interface.keyword'] = 'TSInterfaceKeyword',
+  ['interface.name'] = 'TSInterfaceName',
+  ['local.name'] = 'TSLocalName',
+  ['namespace.keyword'] = 'TSNamespaceKeyword',
+  ['namespace.name'] = 'TSNamespaceName'
 }
 
 require 'treesitter-context.config'.setup {
