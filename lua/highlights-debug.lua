9a4d6ac855e0342387802f4192bdd30b44533075
Moschcau, Kai <kai.moschcau@sabre.com>
2023-02-01 11:54:11 +0100

    refactor: simplify highlights debug

--- lua/highlights-debug.lua
+++ lua/highlights-debug.lua
@@ -1,61 +1,9 @@
 local M = {}
 
-local function transform_extmark(extmarks, extmark)
-  local _, col = unpack(vim.api.nvim_win_get_cursor(0))
-  local extmark_start = extmark[3]
-  local details = extmark[4]
-  local extmark_end = details.end_col
-
-  if
-    (extmark_start and extmark_start > col)
-    or (extmark_end and extmark_end < col)
-  then
-    return
-  end
-
-  table.insert(extmarks, {
-    hl = details.hl_group,
-    priority = details.priority,
-  })
-end
-
-local function transform_extmarks(results, extmarks, namespace)
-  local transformed = {}
-
-  for _, extmark in ipairs(extmarks) do
-    transform_extmark(transformed, extmark)
-  end
-
-  if #transformed < 1 then
-    return
-  end
-
-  table.insert(results, {
-    extmarks = transformed,
-    namespace = namespace,
-  })
-end
-
-local function get_extmarks_at_cursor()
-  local namespaces = vim.api.nvim_get_namespaces()
-  local line = unpack(vim.api.nvim_win_get_cursor(0))
-  local start = { line - 1, 0 }
-  local end_pos = { line - 1, -1 }
-
-  local extmarks = {}
-  local treesitter_captures = vim.treesitter.get_captures_at_cursor()
-
-  for namespace, id in pairs(namespaces) do
-    transform_extmarks(
-      extmarks,
-      vim.api.nvim_buf_get_extmarks(0, id, start, end_pos, { details = true }),
-      namespace
-    )
-  end
-
+local function get_treesitter_and_semantic_at_cursor()
   return {
-    extmarks = extmarks,
-    treesitter_captures = treesitter_captures,
+    semantic_tokens = vim.lsp.semantic_tokens.get_at_pos(),
+    treesitter_captures = vim.treesitter.get_captures_at_cursor(),
   }
 end
 
@@ -63,9 +11,9 @@ end
 --- @return nil
 M.show_extmarks_at_cursor = function()
   vim.lsp.util.open_floating_preview(
-    vim.split(vim.inspect(get_extmarks_at_cursor()), "\n"),
+    vim.split(vim.inspect(get_treesitter_and_semantic_at_cursor()), "\n"),
     "lua",
-    { border = "rounded", title = "Extmarks" }
+    { border = "rounded", title = "Semantic + Treesitter" }
   )
 end
 
