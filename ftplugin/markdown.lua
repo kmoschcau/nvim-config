4b13f2506f49d5b3abda76944465b5ac6f5959b6
Kai Moschcau <mail@kmoschcau.de>
2023-05-08 19:32:40 +0200

    feat: remove markdown i_C-k mapping

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,3 +1,5 @@
+local ehandler = require("error-handler").handler
+
 -- vim: foldmethod=marker
 -- Markdown file type settings
 
@@ -5,3 +7,10 @@
 -- Neovim options {{{2
 
 vim.opt_local.formatoptions:append "t"
+
+-- plugin configurations {{{1
+
+-- nvim-markdown | ixru/nvim-markdown {{{2
+
+-- Remove the default `insert link` mapping, so that we can use digraphs.
+xpcall(vim.keymap.del, ehandler, "i", "<C-k>", { buffer = true })
