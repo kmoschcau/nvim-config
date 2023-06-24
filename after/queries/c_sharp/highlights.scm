b0cd7a3af43295f709ef966a4f345fae07cf3758
Kai Moschcau <mail@kmoschcau.de>
2023-06-24 21:42:18 +0200

    feat: add highlight queries for C#

---
+++ after/queries/c_sharp/highlights.scm
@@ -0,0 +1,34 @@
+;; extends
+
+; namespaces
+
+(extern_alias_directive
+  "extern" @include
+  "alias" @include
+  name: (_) @namespace)
+
+(using_directive
+  "global" @keyword)
+
+(using_directive
+  name: (identifier) @namespace)
+
+(qualified_name
+  qualifier: (identifier) @namespace)
+
+(file_scoped_namespace_declaration
+  "namespace" @keyword.namespace)
+
+(namespace_declaration
+  "namespace" @keyword.namespace)
+
+; type declarations
+
+(class_declaration
+  "class" @keyword.class)
+
+(interface_declaration
+  "interface" @keyword.interface)
+
+(enum_declaration
+  "enum" @keyword.enum)
