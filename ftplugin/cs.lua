0c01cbee706a7c0023f1d689453daa8e62fe3f92
Kai Moschcau <mail@kmoschcau.de>
2023-06-24 21:42:04 +0200

    feat: add more default C# options

--- ftplugin/cs.lua
+++ ftplugin/cs.lua
@@ -4,4 +4,8 @@
 -- general Neovim settings {{{1
 -- Neovim options {{{2
 
+vim.opt_local.fixendofline = false
+vim.opt_local.fileformat = "dos"
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
 vim.opt_local.textwidth = 120
