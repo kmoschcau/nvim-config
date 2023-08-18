ddf067e139e511afb8e47ba8f8b9c164c6d5616b
Kai Moschcau <kai.moschcau@blecon.de>
2023-08-18 09:34:49 +0200

    feat(startup): lazy load TS playground

--- lua/plugin-management/plugins/nvim-treesitter.lua
+++ lua/plugin-management/plugins/nvim-treesitter.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  dependencies = "nvim-treesitter/playground",
   build = function()
     require("nvim-treesitter.install").update { with_sync = true }
   end,
