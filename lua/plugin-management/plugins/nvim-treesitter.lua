7c3b0bbae426341b3fe33c5224970280bd4cd617
Kai Moschcau <kai.moschcau@blecon.de>
2023-08-31 09:02:01 +0200

    fix(treesitter): build not doing anything

--- lua/plugin-management/plugins/nvim-treesitter.lua
+++ lua/plugin-management/plugins/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = function()
-    require("nvim-treesitter.install").update { with_sync = true }
+    require("nvim-treesitter.install").update { with_sync = true }()
   end,
   main = "nvim-treesitter.configs",
   opts = {
