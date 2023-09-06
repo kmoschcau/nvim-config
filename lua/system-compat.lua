3b286bf56ab84bda6eda1e45ed06eb9b0118ef31
Kai Moschcau <kai.moschcau@blecon.de>
2023-09-06 10:13:55 +0200

    feat(compat): add WSL detection and missing wslu check

--- lua/system-compat.lua
+++ lua/system-compat.lua
@@ -23,9 +23,31 @@ M.get_browser_command = function()
 
   if vim.fn.executable "wslview" > 0 then
     return "wslview"
+  elseif M.is_running_in_wsl() then
+    vim.notify(
+      'Could not find "wslview", despite running in WSL.'
+        .. ' Did you forget to install "wslu"?',
+      vim.log.levels.WARN
+    )
   end
 
   return nil
+end
+
+--- Try to determine whether the editor is running in a WSL environment.
+--- @return boolean
+M.is_running_in_wsl = function()
+  if vim.fn.executable "uname" == 0 then
+    return false
+  end
+
+  local result = vim.system({ "uname", "-r" }, { text = true }):wait()
+  if result.code ~= 0 then
+    vim.notify('Calling "uname" failed.', vim.log.levels.ERROR)
+    return false
+  end
+
+  return result.stdout:match "microsoft"
 end
 
 return M
