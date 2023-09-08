d7093debdf7cf7eb66e07488ab1d1d58ea3f38f1
Kai Moschcau <kai.moschcau@blecon.de>
2023-09-08 17:13:15 +0200

    feat(ftplugin): add razor

---
+++ ftplugin/razor.lua
@@ -0,0 +1,13 @@
+-- vim: foldmethod=marker
+-- Razor template file type settings
+
+-- general Neovim settings {{{1
+-- Neovim options {{{2
+
+vim.opt_local.fixendofline = false
+if vim.opt_local.modifiable:get() then
+  vim.opt_local.fileformat = "dos"
+end
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.textwidth = 120
