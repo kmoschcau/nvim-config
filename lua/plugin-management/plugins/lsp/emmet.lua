ee1d7547785b0e755c4af0ae92ff6aacec06fecf
Kai Moschcau <kai.moschcau@blecon.de>
2023-09-10 13:14:44 +0200

    feat(emmet): enable in razor

--- lua/plugin-management/plugins/lsp/emmet.lua
+++ lua/plugin-management/plugins/lsp/emmet.lua
@@ -4,6 +4,22 @@ local lspconfig = require "lspconfig"
 lspconfig.emmet_language_server.setup {
   capabilities = common.capabilities,
   handlers = common.handlers,
+  filetypes = {
+    "astro",
+    "css",
+    "eruby",
+    "html",
+    "htmldjango",
+    "javascriptreact",
+    "less",
+    "pug",
+    "razor",
+    "sass",
+    "scss",
+    "svelte",
+    "typescriptreact",
+    "vue",
+  },
   init_options = {
     preferences = {
       output = {
