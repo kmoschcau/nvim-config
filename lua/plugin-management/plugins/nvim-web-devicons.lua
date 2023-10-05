6176cc01d23168b4be19f56ac3fd50d2037de9df
Kai Moschcau <kai.moschcau@blecon.de>
2023-10-05 15:06:40 +0200

    feat(devicons): expand devicons overrides

--- lua/plugin-management/plugins/nvim-web-devicons.lua
+++ lua/plugin-management/plugins/nvim-web-devicons.lua
@@ -1,3 +1,9 @@
+local dotnet_icon = "󰪮"
+local dotnet_color = "#512bd4"
+
+local font_icon = ""
+local font_color = "#2f2f2f"
+
 return {
   "nvim-tree/nvim-web-devicons",
   lazy = true,
@@ -5,30 +11,61 @@ return {
     default = true,
     strict = true,
     override_by_filename = {
-      ["NuGet"] = {
-        icon = "󰪮",
-        color = "#443ad1",
+      ["NuGet.Config"] = {
+        icon = dotnet_icon,
+        color = "#004880",
         name = "NuGetConfig",
       },
     },
     override_by_extension = {
+      azcli = {
+        icon = "",
+        color = "#0078d4",
+        name = "AzureCli",
+      },
+      cshtml = {
+        icon = "󱦗",
+        color = dotnet_color,
+        name = "RazorPage",
+      },
       csproj = {
-        icon = "󰪮",
-        color = "#443ad1",
-        name = "DotNet",
+        icon = dotnet_icon,
+        color = dotnet_color,
+        name = "CSharpProject",
+      },
+      eot = {
+        icon = font_icon,
+        color = font_color,
+        name = "EmbeddedOpenTypeFont",
       },
       nswag = {
         icon = "",
-        color = "#cbcb41",
+        color = "#85ea2d",
         name = "Nswag",
       },
+      razor = {
+        icon = "󱦘",
+        color = dotnet_color,
+        name = "RazorComponent",
+      },
       ttf = {
-        icon = "",
-        name = "TrueType",
+        icon = font_icon,
+        color = font_color,
+        name = "TrueTypeFont",
+      },
+      woff = {
+        icon = font_icon,
+        color = font_color,
+        name = "WebOpenFontFormat",
+      },
+      woff2 = {
+        icon = font_icon,
+        color = font_color,
+        name = "WebOpenFontFormat",
       },
       xaml = {
         icon = "󰙳",
-        color = "#443ad1",
+        color = dotnet_color,
         name = "Xaml",
       },
     },
