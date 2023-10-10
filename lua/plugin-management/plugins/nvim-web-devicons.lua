2f6fb73f9f3e0a7306c85c36423728eb6d8c7922
Kai Moschcau <kai.moschcau@blecon.de>
2023-10-10 13:13:07 +0200

    chore(devicons): update with added icons PR

--- lua/plugin-management/plugins/nvim-web-devicons.lua
+++ lua/plugin-management/plugins/nvim-web-devicons.lua
@@ -1,9 +1,3 @@
-local dotnet_icon = "󰪮"
-local dotnet_color = "#512bd4"
-
-local font_icon = ""
-local font_color = "#2f2f2f"
-
 return {
   "nvim-tree/nvim-web-devicons",
   lazy = true,
@@ -12,61 +6,9 @@ return {
     strict = true,
     override_by_filename = {
       ["NuGet.Config"] = {
-        icon = dotnet_icon,
+        icon = "󰪮",
         color = "#004880",
         name = "NuGetConfig",
-      },
-    },
-    override_by_extension = {
-      azcli = {
-        icon = "",
-        color = "#0078d4",
-        name = "AzureCli",
-      },
-      cshtml = {
-        icon = "󱦗",
-        color = dotnet_color,
-        name = "RazorPage",
-      },
-      csproj = {
-        icon = dotnet_icon,
-        color = dotnet_color,
-        name = "CSharpProject",
-      },
-      eot = {
-        icon = font_icon,
-        color = font_color,
-        name = "EmbeddedOpenTypeFont",
-      },
-      nswag = {
-        icon = "",
-        color = "#85ea2d",
-        name = "Nswag",
-      },
-      razor = {
-        icon = "󱦘",
-        color = dotnet_color,
-        name = "RazorComponent",
-      },
-      ttf = {
-        icon = font_icon,
-        color = font_color,
-        name = "TrueTypeFont",
-      },
-      woff = {
-        icon = font_icon,
-        color = font_color,
-        name = "WebOpenFontFormat",
-      },
-      woff2 = {
-        icon = font_icon,
-        color = font_color,
-        name = "WebOpenFontFormat",
-      },
-      xaml = {
-        icon = "󰙳",
-        color = dotnet_color,
-        name = "Xaml",
       },
     },
   },
