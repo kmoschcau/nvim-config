95fe0f8e6d6dbcebf240f3e59985845e4a333035
Kai Moschcau <kai.moschcau@blecon.de>
2023-10-23 09:14:16 +0200

    feat(plugins): add undotree

---
+++ lua/plugin-management/plugins/undotree.lua
@@ -0,0 +1,12 @@
+return {
+  "mbbill/undotree",
+  cmd = "UndotreeToggle",
+  keys = {
+    {
+      "<F5>",
+      ":UndotreeToggle<cr>",
+      desc = "Toggle the Undotree window.",
+      silent = true,
+    },
+  },
+}
