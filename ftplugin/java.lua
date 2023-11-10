a2a44f2ea6f3476066086d3df63ddf0d36c352d1
Kai Moschcau <kai.moschcau@blecon.de>
2023-11-10 09:50:52 +0100

    fix: wrong reference in java ftplugin

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -9,4 +9,4 @@ vim.opt_local.textwidth = 100
 -- plugin configurations {{{1
 -- jdtls | mfussenegger/nvim-jdtls {{{2
 
-require("plugins.config.lsp.jdtls").start_or_attach()
+require("plugin-management.plugins.lsp.jdtls").start_or_attach()
