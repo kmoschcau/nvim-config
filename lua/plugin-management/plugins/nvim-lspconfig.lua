ac78f9e90a7aa5a8b269722b6e137e3af1005cd6
Kai Moschcau <kai.moschcau@blecon.de>
2023-11-12 13:01:54 +0100

    feat: add gotmpl and heml support

--- lua/plugin-management/plugins/nvim-lspconfig.lua
+++ lua/plugin-management/plugins/nvim-lspconfig.lua
@@ -19,6 +19,7 @@ return {
     local simple_servers = {
       "bashls",
       "gradle_ls",
+      "helm_ls",
       "jedi_language_server",
       "jqls",
       "kotlin_language_server",
--- lua/plugin-management/plugins/nvim-treesitter.lua
+++ lua/plugin-management/plugins/nvim-treesitter.lua
@@ -3,65 +3,78 @@ return {
   build = function()
     require("nvim-treesitter.install").update { with_sync = true }()
   end,
-  main = "nvim-treesitter.configs",
-  opts = {
-    ensure_installed = {
-      -- This should only include what should be there on first setup, not what
-      -- I end up using over time.
-      "bash",
-      "comment",
-      "diff",
-      "fish",
-      "git_config",
-      "git_rebase",
-      "gitattributes",
-      "gitcommit",
-      "gitignore",
-      "gpg",
-      "json",
-      "jsonc",
-      "lua",
-      "luadoc",
-      "luap",
-      "markdown",
-      "markdown_inline",
-      "regex",
-      "toml",
-      "vim",
-      "vimdoc",
-      "yaml",
-    },
-    auto_install = true,
-
-    -- modules below
-
-    -- windwp/nvim-ts-autotag
-    autotag = {
-      enable = true,
-    },
-
-    -- RRethy/nvim-treesitter-endwise
-    endwise = {
-      enable = true,
-    },
-
-    -- nvim-treesitter/nvim-treesitter
-    highlight = {
-      enable = true,
-    },
-
-    -- nvim-treesitter/nvim-treesitter
-    indent = {
-      enable = true,
-      disable = {
-        "javascript",
-        "typescript",
+  config = function()
+    local parser_config =
+      require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.gotmpl = {
+      install_info = {
+        url = "https://github.com/ngalaiko/tree-sitter-go-template",
+        files = { "src/parser.c" },
       },
-    },
+    }
+    vim.treesitter.language.register("gotmpl", "helm")
 
-    -- andymass/vim-matchup
-    matchup = {
-      enable = true,
-    },
-  },
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = {
+        -- This should only include what should be there on first setup, not what
+        -- I end up using over time.
+        "bash",
+        "comment",
+        "diff",
+        "fish",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "gotmpl",
+        "gpg",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "regex",
+        "toml",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+      auto_install = true,
+
+      -- modules below
+
+      -- windwp/nvim-ts-autotag
+      autotag = {
+        enable = true,
+      },
+
+      -- RRethy/nvim-treesitter-endwise
+      endwise = {
+        enable = true,
+      },
+
+      -- nvim-treesitter/nvim-treesitter
+      highlight = {
+        enable = true,
+      },
+
+      -- nvim-treesitter/nvim-treesitter
+      indent = {
+        enable = true,
+        disable = {
+          "javascript",
+          "typescript",
+        },
+      },
+
+      -- andymass/vim-matchup
+      matchup = {
+        enable = true,
+      },
+    }
+  end,
 }
