7f92a4f6af3698693d5fc66ad76189d7772e3d83
Kai Moschcau <mail@kmoschcau.de>
2024-01-04 17:09:56 +0100

    feat(lazy): setup for plugin dev

--- lua/plugin-management/lazy-config.lua
+++ lua/plugin-management/lazy-config.lua
@@ -1,4 +1,8 @@
 require("lazy").setup("plugin-management.plugins", {
+  dev = {
+    path = "~/Code",
+    patterns = { "kmoschcau" },
+  },
   ui = {
     border = "rounded",
     browser = require("system-compat").get_browser_command(),
