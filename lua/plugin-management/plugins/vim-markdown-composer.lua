9172b615b92d287e18edc2723c3a8f3d81b3eacc
Kai Moschcau <mail@kmoschcau.de>
2024-01-04 13:01:45 +0100

    refactor(lazy): set vim vars in init instead of config

--- lua/plugin-management/plugins/vim-markdown-composer.lua
+++ lua/plugin-management/plugins/vim-markdown-composer.lua
@@ -1,11 +1,8 @@
 return {
   "euclio/vim-markdown-composer",
   build = "cargo build --release",
-  config = function()
-    -- Whether the server should automatically start when a markdown file is opened.
+  init = function()
     vim.g.markdown_composer_autostart = 0
-
-    -- Pick the right browser for the system we are running in
     vim.g.markdown_composer_browser =
       require("system-compat").get_browser_command()
   end,
