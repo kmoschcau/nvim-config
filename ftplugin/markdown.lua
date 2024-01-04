ef42a0fb20b7cc90c74e92a0476bd47d8a13228a
Kai Moschcau <mail@kmoschcau.de>
2024-01-04 12:53:42 +0100

    fix(markdown): i_<C-K> not being digraphs

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -5,10 +5,3 @@
 -- Neovim options {{{2
 
 vim.opt_local.formatoptions:append "t"
-
--- plugin configurations {{{1
-
--- nvim-markdown | ixru/nvim-markdown {{{2
-
--- Remove the default `insert link` mapping, so that we can use digraphs.
-pcall(vim.keymap.del, "i", "<C-k>", { buffer = true })
