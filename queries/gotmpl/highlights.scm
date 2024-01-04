085d04828a6a1282ba6218cf6ebc54aaf91542d8
Kai Moschcau <mail@kmoschcau.de>
2024-01-04 13:34:48 +0100

    feat(treesitter): increase gotmpl highlights priority

--- queries/gotmpl/highlights.scm
+++ queries/gotmpl/highlights.scm
@@ -1,84 +1,91 @@
 ; Identifiers
 
-[
-    (field)
-    (field_identifier)
-] @property
+([
+  (field)
+  (field_identifier)
+  ] @property (#set! priority 101))
 
-(variable) @variable
+((variable) @variable (#set! priority 101))
 
 ; Function calls
 
 (function_call
-  function: (identifier) @function)
+  function: (identifier) @function (#set! priority 101))
 
 (method_call
   method: (selector_expression
-    field: (field_identifier) @method))
+            field: (field_identifier) @method (#set! priority 101)))
 
 ; Operators
 
-"|" @operator
-":=" @operator
+([
+  "|"
+  ":="
+  ] @operator (#set! priority 101))
 
 ; Builtin functions
 
 ((identifier) @function.builtin
- (#match? @function.builtin "^(and|call|html|index|slice|js|len|not|or|print|printf|println|urlquery|eq|ne|lt|ge|gt|ge)$"))
+              (#set! priority 101)
+              (#match? @function.builtin "^(and|call|html|index|slice|js|len|not|or|print|printf|println|urlquery|eq|ne|lt|ge|gt|ge)$"))
 
 ; Delimiters
 
-"." @punctuation.delimiter
-"," @punctuation.delimiter
+([
+  "."
+  ","
+  ] @punctuation.delimiter (#set! priority 101))
 
-"{{" @punctuation.bracket
-"}}" @punctuation.bracket
-"{{-" @punctuation.bracket
-"-}}" @punctuation.bracket
-")" @punctuation.bracket
-"(" @punctuation.bracket
+([
+  "{{"
+  "}}"
+  "{{-"
+  "-}}"
+  ")"
+  "("
+  "{{"
+  ] @punctuation.bracket (#set! priority 101))
 
 ; Keywords
 
-[
-    "else"
-    "else if"
-    "if"
-    "with"
-] @conditional
+([
+  "else"
+  "if"
+  ] @conditional (#set! priority 101))
 
-[
-    "range"
-    "end"
-    "template"
-    "define"
-    "block"
-] @keyword
+([
+  "range"
+  "with"
+  "end"
+  "template"
+  "define"
+  "block"
+  ] @keyword (#set! priority 101))
 
 ; Literals
 
-[
+([
   (interpreted_string_literal)
   (raw_string_literal)
   (rune_literal)
-] @string
+  ] @string (#set! priority 101))
 
-(escape_sequence) @string.special
+((escape_sequence) @string.special (#set! priority 101))
 
-[
+([
   (int_literal)
   (float_literal)
   (imaginary_literal)
-] @number
+  ] @number (#set! priority 101))
 
-[
-    (true)
-    (false)
-] @boolean
+([
+  (true)
+  (false)
+  ] @boolean (#set! priority 101))
 
-[
+([
   (nil)
-] @constant.builtin
+  ] @constant.builtin (#set! priority 101))
 
-(comment) @comment
-(ERROR) @error
+((comment) @comment (#set! priority 101))
+((ERROR) @error (#set! priority 101))
