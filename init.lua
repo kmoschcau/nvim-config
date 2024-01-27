8bf0702847e631d493e4509e9b4105422f2ce997
Kai Moschcau <mail@kmoschcau.de>
2024-01-27 22:43:06 +0100

    feat!: set foldtext to empty string to use highlighting

--- init.lua
+++ init.lua
@@ -78,7 +78,7 @@ vim.opt.fillchars = { diff = " " }
 vim.o.foldenable = false
 vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.o.foldmethod = "expr"
-vim.o.foldtext = "v:lua.vim.treesitter.foldtext()"
+vim.o.foldtext = ""
 vim.o.formatoptions = "cro/qnlj"
 vim.o.guicursor = table.concat({
   table.concat({
