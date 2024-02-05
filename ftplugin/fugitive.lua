307512ba6ea7fff5e83ff5bd4c9a5fce0315448c
Kai Moschcau <kai.moschcau@blecon.de>
2024-02-05 14:59:54 +0100

    feat(fugitive): add keymap to run `commit --no-verify`

--- ftplugin/fugitive.lua
+++ ftplugin/fugitive.lua
@@ -7,3 +7,9 @@
 -- Set the color column to be two more than textwidth, since in diffs we usually
 -- have one column more at the beginning of a line.
 vim.opt_local.colorcolumn = "+2"
+
+vim.keymap.set("n", "CC", ":G commit --no-verify<cr>", {
+  buffer = true,
+  silent = true,
+  desc = "Commit without git hooks."
+})
