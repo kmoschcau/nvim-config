662e45137f99f54c8e369c1f346b466ff997f04a
Kai Moschcau <kai.moschcau@blecon.de>
2024-02-13 13:34:59 +0100

    feat(snippets): add a gitcommit snippet for plugin update chores

--- lua/snippets/gitcommit.lua
+++ lua/snippets/gitcommit.lua
@@ -1,4 +1,12 @@
 local ls = require "luasnip"
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
 
 ls.add_snippets("gitcommit", {
+  s({
+    name = "chore: Plugins update",
+    trig = "cpu",
+    desc = "A conventional commit subject for a chore commit, updating plugins",
+  }, { t { "chore(plugins): update", "", "" }, i(0) }),
 })
