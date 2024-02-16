fc8297d65618764d80cc0138f16fe4af21472201
Kai Moschcau <kai.moschcau@blecon.de>
2024-02-16 14:29:07 +0100

    feat(kotlin): add underscore to module names query

--- queries/kotlin/highlights.scm
+++ queries/kotlin/highlights.scm
@@ -7,5 +7,5 @@
 
 (import_header
   (identifier
-    (simple_identifier) @module (#match? @module "^[a-z0-9]+$")
+    (simple_identifier) @module (#match? @module "^[_a-z0-9]+$")
     (simple_identifier) .))
