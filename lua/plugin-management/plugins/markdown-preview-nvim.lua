cba2dfd8851c774f97b45e6eca9dd46815f08446
Kai Moschcau <mail@kmoschcau.de>
2024-02-17 18:36:11 +0100

    feat(plugins)!: replace markdown-composer with markdown-preview

---
+++ lua/plugin-management/plugins/markdown-preview-nvim.lua
@@ -0,0 +1,13 @@
+--- @type LazyPluginSpec
+return {
+  "iamcco/markdown-preview.nvim",
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = "markdown",
+  config = function()
+    vim.g.mkdp_auto_close = 0
+    vim.g.mkdp_combine_preview = 1
+  end
+}
