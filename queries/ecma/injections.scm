a38e021e8e1ea92ea844fc207dee8991c9f7ebe5
Kai Moschcau <kai.moschcau@blecon.de>
2024-02-23 15:53:11 +0100

    fix(query): ecmascript HTML injection with trim

--- queries/ecma/injections.scm
+++ queries/ecma/injections.scm
@@ -4,11 +4,12 @@
 (assignment_expression
   left: (member_expression
           property: (property_identifier) @_prop
-          (#any-of? @_prop "innerHTML" "outerHTML"))
+          (#any-of? @_prop "outerHTML" "innerHTML"))
   right: (call_expression
            function: (member_expression
                        object: (template_string) @injection.content
                        property: (property_identifier) @_prop2
                        (#any-of? @_prop2 "trim")
                        (#offset! @injection.content 0 1 0 -1)
+                       (#set! injection.include-children)
                        (#set! injection.language "html"))))
