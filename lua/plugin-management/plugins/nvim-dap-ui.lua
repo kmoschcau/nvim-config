4f6bd6cb9480bfb1d756110cde74cb7a12e7c1c9
Kai Moschcau <kai.moschcau@blecon.de>
2024-03-19 09:23:32 +0100

    fix(plugins): add missing dependency of nvim-dap-ui

--- lua/plugin-management/plugins/nvim-dap-ui.lua
+++ lua/plugin-management/plugins/nvim-dap-ui.lua
@@ -1,6 +1,9 @@
 --- @type LazyPluginSpec
 return {
   "rcarriga/nvim-dap-ui",
-  dependencies = "mfussenegger/nvim-dap",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    "nvim-neotest/nvim-nio",
+  },
   config = true,
 }
