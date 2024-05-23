b210143bfcdb068941153e017a5c07c3cc1959c8
Kai Moschcau <kai.moschcau@blecon.de>
2024-05-23 14:39:00 +0200

    refactor(snippets): rewrite gitcommit to use fmt

--- lua/snippets/gitcommit.lua
+++ lua/snippets/gitcommit.lua
@@ -1,12 +1,23 @@
 local ls = require "luasnip"
 local s = ls.snippet
-local t = ls.text_node
 local i = ls.insert_node
 
+local fmt = require("luasnip.extras.fmt").fmt
+
 ls.add_snippets("gitcommit", {
-  s({
-    name = "chore: Plugins update",
-    trig = "cpu",
-    desc = "A conventional commit subject for a chore commit, updating plugins",
-  }, { t { "chore(plugins): update", "", "" }, i(0) }),
+  s(
+    {
+      name = "chore: Plugins update",
+      trig = "cpu",
+      desc = "A conventional commit subject for a chore commit, updating plugins",
+    },
+    fmt(
+      [[
+        chore(plugins): update
+
+        {}
+      ]],
+      { i(0) }
+    )
+  ),
 })
