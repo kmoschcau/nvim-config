a0e09cb648132ea4141d7e178a5379adf034f953
Kai Moschcau <kai.moschcau@blecon.de>
2024-05-31 10:11:27 +0200

    feat(plugins): add lsplinks.nvim

---
+++ lua/plugin-management/plugins/lsplinks-nvim.lua
@@ -0,0 +1,11 @@
+--- @type LazyPluginSpec
+return {
+  "icholy/lsplinks.nvim",
+  config = function()
+    local lsplinks = require "lsplinks"
+    lsplinks.setup()
+    vim.keymap.set("n", "gx", lsplinks.gx, {
+      desc = "LSPLINKS: Open the current filepath or URL at cursor."
+    })
+  end,
+}
