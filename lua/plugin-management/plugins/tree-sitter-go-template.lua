ab6514af6416065f6439374dd55898ae0b47a108
Kai Moschcau <mail@kmoschcau.de>
2024-06-02 16:19:10 +0200

    feat: remove treesitter-go-template

--- lua/plugin-management/plugins/tree-sitter-go-template.lua
+++
@@ -1,8 +0,0 @@
---- @type LazyPluginSpec
-return {
-  "kmoschcau/tree-sitter-go-template",
-  dependencies = "nvim-treesitter/nvim-treesitter",
-  enabled = true,
-  dev = true,
-  config = true,
-}
