70fe7149a698e7d5a62c511ce1597b1e4a0ecacd
Kai Moschcau <kai.moschcau@blecon.de>
2024-06-03 08:10:02 +0200

    feat(lazy): do not automatically install on start

--- lua/plugin-management/lazy-config.lua
+++ lua/plugin-management/lazy-config.lua
@@ -1,13 +1,16 @@
 require("lazy").setup("plugin-management.plugins", {
+  checker = {
+    enabled = true,
+  },
   dev = {
     path = "~/Code",
     patterns = { "kmoschcau" },
   },
+  install = {
+    missing = false,
+  },
   ui = {
     border = "rounded",
     browser = require("system-compat").get_browser_command(),
-  },
-  checker = {
-    enabled = true,
   },
 })
