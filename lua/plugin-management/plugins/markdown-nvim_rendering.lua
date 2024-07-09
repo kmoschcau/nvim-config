f573d7a4b41b25786f61dc849d938b7016fda3f0
Kai Moschcau <kai.moschcau@blecon.de>
2024-07-09 17:10:27 +0200

    feat(plugins): make mini.icons a dependency of markdown render

--- lua/plugin-management/plugins/markdown-nvim_rendering.lua
+++ lua/plugin-management/plugins/markdown-nvim_rendering.lua
@@ -1,6 +1,7 @@
 --- @type LazyPluginSpec
 return {
   "MeanderingProgrammer/markdown.nvim",
+  dependencies = { "echasnovski/mini.icons" },
   name = "markdown.nvim-render",
   ft = "markdown",
   opts = {
