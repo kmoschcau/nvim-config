2cc24f4a4d54417bb9534f0df11826eb668cd7d6
Kai Moschcau <mail@kmoschcau.de>
2024-08-03 09:23:09 +0200

    refactor(coerce): change setup to built-in default prefixes for keymaps

--- lua/plugin-management/plugins/coerce-nvim.lua
+++ lua/plugin-management/plugins/coerce-nvim.lua
@@ -1,41 +1,12 @@
 --- @type LazyPluginSpec
 return {
   "gregorias/coerce.nvim",
-  config = function()
-    local coerce = require "coerce"
-
-    local selector_m = require "coerce.selector"
-    local transformer_m = require "coerce.transformer"
-
-    coerce.setup {
-      modes = {},
-    }
-
-    coerce.register_mode {
-      vim_mode = "n",
-      keymap_prefix = "<Space>cc",
-      selector = selector_m.select_current_word,
-      transformer = function(selected_region, apply)
-        return require("coerce.coroutine").fire_and_forget(
-          transformer_m.transform_lsp_rename_with_local_failover,
-          selected_region,
-          apply
-        )
-      end,
-    }
-
-    coerce.register_mode {
-      vim_mode = "n",
-      keymap_prefix = "<Space>cr",
-      selector = selector_m.select_with_motion,
-      transformer = transformer_m.transform_local,
-    }
-
-    coerce.register_mode {
-      vim_mode = "x",
-      keymap_prefix = "<Space>cr",
-      selector = selector_m.select_current_visual_selection,
-      transformer = transformer_m.transform_local,
-    }
-  end,
+  --- @type CoerceConfigUser
+  opts = {
+    default_mode_keymap_prefixes = {
+      normal_mode = "<Space>cc",
+      motion_mode = "<Space>cr",
+      visual_mode = "<Space>cr",
+    },
+  },
 }
