9aa50cc97af99a250df0f60b72f92f6c04501350
Kai Moschcau <kai.moschcau@blecon.de>
2024-08-25 18:55:27 +0200

    feat(dap): add C# ASP.NET configuration

--- lua/plugin-management/plugins/nvim-dap.lua
+++ lua/plugin-management/plugins/nvim-dap.lua
@@ -5,11 +5,167 @@ return {
     local dap = require "dap"
     local dapui = require "dapui"
 
+    local action_state = require "telescope.actions.state"
+    local actions = require "telescope.actions"
+    local conf = require("telescope.config").values
+    local finders = require "telescope.finders"
+    local pickers = require "telescope.pickers"
+
+    dap.adapters.netcoredbg = {
+      type = "executable",
+      command = require("system-compat").append_win_ext("netcoredbg", "exe"),
+      args = { "--interpreter=vscode" },
+    }
+
+    --- @type dap.Configuration[]
+    dap.configurations.cs = {
+      {
+        name = "Launch - ASP.NET Core",
+        type = "netcoredbg",
+        request = "launch",
+        justMyCode = false,
+        stopAtEntry = false,
+        env = {
+          ASPNETCORE_ENVIRONMENT = "Development",
+          ASPNETCORE_URLS = "http://localhost:5274",
+        },
+        cwd = function()
+          return coroutine.create(function(dap_cwd_coro)
+            pickers
+              .new({}, {
+                prompt_title = "Project working directory (via csproj)",
+                finder = finders.new_oneshot_job(
+                  { "fd", "--extension", "csproj" },
+                  {}
+                ),
+                sorter = conf.generic_sorter {},
+                attach_mappings = function(buf, map)
+                  actions.select_default:replace(function()
+                    actions.close(buf)
+                    local work_dir = vim.fs.normalize(
+                      vim.fs.dirname(
+                        vim.fs.joinpath(
+                          vim.fn.getcwd(),
+                          action_state.get_selected_entry()[1]
+                        )
+                      )
+                    )
+                    coroutine.resume(dap_cwd_coro, work_dir)
+                  end)
+
+                  map({ "i", "n" }, "<C-c>", function(bufnr)
+                    actions.close(bufnr)
+                    coroutine.resume(dap_cwd_coro, dap.ABORT)
+                  end)
+
+                  return true
+                end,
+              })
+              :find()
+          end)
+        end,
+        program = function()
+          return coroutine.create(function(dap_program_coro)
+            local function select_dll()
+              pickers
+                .new({}, {
+                  prompt_title = "dll to debug",
+                  finder = finders.new_oneshot_job({
+                    "fd",
+                    "--full-path",
+                    "--no-ignore",
+                    "--extension",
+                    "dll",
+                    "bin[\\\\/]Debug",
+                  }, {}),
+                  sorter = conf.generic_sorter {},
+                  attach_mappings = function(buf, map)
+                    actions.select_default:replace(function()
+                      actions.close(buf)
+                      local dll = vim.fs.normalize(
+                        vim.fs.joinpath(
+                          vim.fn.getcwd(),
+                          action_state.get_selected_entry()[1]
+                        )
+                      )
+                      coroutine.resume(dap_program_coro, dll)
+                    end)
+
+                    map({ "i", "n" }, "<C-c>", function(bufnr)
+                      actions.close(bufnr)
+                      coroutine.resume(dap_program_coro, dap.ABORT)
+                    end)
+
+                    return true
+                  end,
+                })
+                :find()
+            end
+
+            if
+              vim.fn.confirm("Should I recompile first?", "&yes\n&no", 2) == 1
+            then
+              pickers
+                .new({}, {
+                  prompt_title = "csproj to build",
+                  finder = finders.new_oneshot_job(
+                    { "fd", "--extension", "csproj" },
+                    {}
+                  ),
+                  sorter = conf.generic_sorter {},
+                  attach_mappings = function(buf, map)
+                    actions.select_default:replace(function()
+                      actions.close(buf)
+                      local csproj = vim.fs.normalize(
+                        vim.fs.joinpath(
+                          vim.fn.getcwd(),
+                          action_state.get_selected_entry()[1]
+                        )
+                      )
+                      local result = vim
+                        .system({
+                          "dotnet",
+                          "build",
+                          csproj,
+                          "--configuration",
+                          "Debug",
+                        }, { text = true })
+                        :wait()
+                      if result.code == 0 then
+                        vim.notify("✔", vim.log.levels.INFO, {
+                          title = "Build",
+                        })
+                        select_dll()
+                      else
+                        vim.notify("❌", vim.log.levels.ERROR, {
+                          title = "Build",
+                        })
+                        coroutine.resume(dap_program_coro, dap.ABORT)
+                      end
+                    end)
+
+                    map({ "i", "n" }, "<C-c>", function(bufnr)
+                      actions.close(bufnr)
+                      coroutine.resume(dap_program_coro, dap.ABORT)
+                    end)
+
+                    return true
+                  end,
+                })
+                :find()
+            else
+              select_dll()
+            end
+          end)
+        end,
+      },
+    }
+
     dap.configurations.java = {
       {
+        name = "Debug (Attach) - Remote",
         type = "java",
         request = "attach",
-        name = "Debug (Attach) - Remote",
         hostName = "127.0.0.1",
         port = 5005,
       },
@@ -79,23 +235,23 @@ return {
     local debug = require("symbols").debug
     vim.fn.sign_define("DapBreakpoint", {
       text = debug.breakpoint.normal,
-      texthl = "Material_SignBreakpoint",
+      texthl = "DapBreakpoint",
     })
     vim.fn.sign_define("DapBreakpointCondition", {
       text = debug.breakpoint.conditional,
-      texthl = "Material_SignBreakpointConditional",
+      texthl = "DapBreakpointCondition",
     })
     vim.fn.sign_define("DapLogPoint", {
       text = debug.breakpoint.log,
-      texthl = "Material_SignLogpoint",
+      texthl = "DapLogPoint",
     })
     vim.fn.sign_define("DapStopped", {
       text = debug.current_frame,
-      texthl = "Material_SignCurrentFrame",
+      texthl = "DapStopped",
     })
     vim.fn.sign_define("DapBreakpointRejected", {
       text = debug.breakpoint.rejected,
-      texthl = "Material_SignBreakpointRejected",
+      texthl = "DapBreakpointRejected",
     })
   end,
 }
