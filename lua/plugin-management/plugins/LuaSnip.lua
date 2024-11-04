2ade3dddfe3991689a1e5e532b0cf64d01d44e4f
Kai Moschcau <kai.moschcau@blecon.de>
2024-11-04 09:32:07 +0100

    feat(plugins): use luarocks jsregexp for LuaSnip

--- lua/plugin-management/plugins/LuaSnip.lua
+++ lua/plugin-management/plugins/LuaSnip.lua
@@ -2,8 +2,7 @@
 --- @type LazyPluginSpec
 return {
   "L3MON4D3/LuaSnip",
-  dependencies = "rafamadriz/friendly-snippets",
-  build = "make install_jsregexp",
+  dependencies = { "rafamadriz/friendly-snippets" },
   config = function()
     require("luasnip.loaders.from_vscode").lazy_load()
 
