b47aa10ca5b4c1a1c5e5b530210902e5218c9bba
Kai Moschcau <kai.moschcau@blecon.de>
2024-11-05 13:58:20 +0100

    feat(lsp): add mason install for roslyn and rzls

--- lua/plugin-management/plugins/lsp/rzls.lua
+++ lua/plugin-management/plugins/lsp/rzls.lua
@@ -1,5 +1,4 @@
 require("rzls").setup {
   capabilities = require("lsp.common").capabilities,
   on_attach = require "lsp.attach",
-  path = "C:/Users/KaiMoschcau/Code/razor/artifacts/bin/rzls/Debug/net8.0/rzls.dll",
 }
--- lua/plugin-management/plugins/mason-nvim.lua
+++ lua/plugin-management/plugins/mason-nvim.lua
@@ -4,6 +4,10 @@ return {
   "williamboman/mason.nvim",
   --- @type MasonSettings
   opts = {
+    registries = {
+      "github:mason-org/mason-registry",
+      "github:Crashdummyy/mason-registry",
+    },
     ui = {
       border = "rounded",
     },
