75b0901c7970ae6ff274cb0e5d06a56a9fa9cebd
Kai Moschcau <kai.moschcau@blecon.de>
2024-11-06 07:38:04 +0100

    feat: remove explicit nu treesitter plugin
    nu is now included in nvim-treesitter.

--- lua/plugin-management/plugins/tree-sitter-nu.lua
+++
@@ -1,9 +0,0 @@
--- selene: allow(mixed_table)
---- @type LazyPluginSpec
-return {
-  "nushell/tree-sitter-nu",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-  },
-  build = ":TSUpdate nu",
-}
