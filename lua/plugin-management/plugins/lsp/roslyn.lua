d128536db96456e053cac5092672331cf24f1308
Kai Moschcau <kai.moschcau@blecon.de>
2024-11-07 09:48:20 +0100

    feat(lsp): expand roslyn/rzls integration and use mason packages

--- lua/plugin-management/plugins/lsp/roslyn.lua
+++ lua/plugin-management/plugins/lsp/roslyn.lua
@@ -47,14 +47,58 @@ local function monkey_patch_semantic_tokens(client)
   end
 end
 
-require("roslyn").setup {
+local mason_registry = require "mason-registry"
+
+--- @type string[]
+local args = {
+  "--logLevel=Information",
+  "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+}
+
+local rzls_package = mason_registry.get_package "rzls"
+if rzls_package:is_installed() then
+  local rzls_path = vim.fs.joinpath(rzls_package:get_install_path(), "libexec")
+  table.insert(
+    args,
+    "--razorSourceGenerator="
+      .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll")
+  )
+  table.insert(
+    args,
+    "--razorDesignTimePath="
+      .. vim.fs.joinpath(
+        rzls_path,
+        "Targets",
+        "Microsoft.NET.Sdk.Razor.DesignTime.targets"
+      )
+  )
+end
+
+--- @type RoslynNvimConfig
+local config = {
+  args = args,
   config = {
     capabilities = require("lsp.common").capabilities,
-    -- handlers = require "rzls.roslyn_handlers",
+    handlers = require "rzls.roslyn_handlers",
     on_attach = function(client, bufnr)
       require "lsp.attach"(client, bufnr)
 
       monkey_patch_semantic_tokens(client)
     end,
   },
+  filewatching = false,
 }
+
+local roslyn_package = mason_registry.get_package "roslyn"
+if roslyn_package:is_installed() then
+  config.exe = {
+    "dotnet",
+    vim.fs.joinpath(
+      roslyn_package:get_install_path(),
+      "libexec",
+      "Microsoft.CodeAnalysis.LanguageServer.dll"
+    ),
+  }
+end
+
+require("roslyn").setup(config)
