615873d9f5c6047669a24198660c4d24ded63d1c
Kai Moschcau <kai.moschcau@blecon.de>
2024-11-14 13:06:16 +0100

    fix: disable trim.nvim, as it breaks last substitution

--- lua/plugin-management/plugins/trim-nvim.lua
+++ lua/plugin-management/plugins/trim-nvim.lua
@@ -2,6 +2,9 @@
 --- @type LazyPluginSpec
 return {
   "cappyzawa/trim.nvim",
+  -- NOTE: This breaks last substitution functionality.
+  -- see: https://github.com/cappyzawa/trim.nvim/issues/27
+  enabled = false,
   opts = {
     ft_blocklist = { "markdown" },
   },
