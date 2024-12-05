88febe1569e4978d338b562584b7cae3cc806f7c
Kai Moschcau <kai.moschcau@blecon.de>
2024-12-05 09:46:09 +0100

    feat: remove now unnecessary TermOpen autocmd settings

--- init.lua
+++ init.lua
@@ -194,9 +194,6 @@ vim.api.nvim_create_autocmd("TermOpen", {
   group = augroup,
   callback = function()
     vim.opt_local.colorcolumn = { "" }
-    vim.opt_local.number = false
-    vim.opt_local.relativenumber = false
-    vim.opt_local.signcolumn = "no"
     vim.opt_local.spell = false
   end,
 })
