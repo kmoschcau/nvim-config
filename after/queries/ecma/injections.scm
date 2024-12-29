eba2e14ce948006485aa9af44137eb10a878543c
Kai Moschcau <mail@kmoschcau.de>
2024-12-29 14:46:10 +0100

    feat(queries): add insertAdjacentHTML injection

--- after/queries/ecma/injections.scm
+++ after/queries/ecma/injections.scm
@@ -13,3 +13,24 @@
       (#offset! @injection.content 0 1 0 -1)
       (#set! injection.include-children)
       (#set! injection.language "html"))))
+
+; el.insertAdjacentHTML("…", `<html>`)
+(call_expression
+  function: (member_expression
+    property: (property_identifier) @_prop
+    (#match? @_prop "insertAdjacentHTML"))
+  arguments: (arguments
+    (template_string) @injection.content)
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children)
+  (#set! injection.language "html"))
+
+; el.insertAdjacentHTML("…", "<html>")
+(call_expression
+  function: (member_expression
+    property: (property_identifier) @_prop
+    (#match? @_prop "insertAdjacentHTML"))
+  arguments: (arguments
+    (string
+      (string_fragment) @injection.content) .)
+  (#set! injection.language "html"))
