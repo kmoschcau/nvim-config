73bc839f805cb6d64e65f403ad4411924b073ac9
Kai Moschcau <kai.moschcau@blecon.de>
2025-01-09 17:20:43 +0100

    feat: add mini snippets

--- lua/plugin-management/plugins/nvim-autopairs.lua
+++ lua/plugin-management/plugins/nvim-autopairs.lua
@@ -3,4 +3,11 @@
 return {
   "windwp/nvim-autopairs",
   config = true,
+  init = function()
+    require("nvim-autopairs").add_rules {
+      require "nvim-autopairs.rule"("{{", "  }", "vue")
+        :set_end_pair_length(2)
+        :with_pair(require("nvim-autopairs.ts-conds").is_ts_node "text"),
+    }
+  end,
 }
