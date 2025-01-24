d799d6b089fe49b179554d7baf05f4cdff340211
Kai Moschcau <kai.moschcau@blecon.de>
2025-01-24 08:32:13 +0100

    feat(plugins)!: remove stand and hydrate plugins

--- lua/plugin-management/plugins/hydrate-nvim.lua
+++
@@ -1,9 +0,0 @@
--- selene: allow(mixed_table)
---- @type LazyPluginSpec
-return {
-  "stefanlogue/hydrate.nvim",
-  --- @type hydrate.config.Opts
-  opts = {
-    persist_timer = true,
-  },
-}
--- lua/plugin-management/plugins/stand-nvim.lua
+++
@@ -1,6 +0,0 @@
--- selene: allow(mixed_table)
---- @type LazyPluginSpec
-return {
-  "mvllow/stand.nvim",
-  config = true,
-}
