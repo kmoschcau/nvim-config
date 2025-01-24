1bd1f75c0e43d8475dd09d54ec88eaad88a4d953
Kai Moschcau <kai.moschcau@blecon.de>
2025-01-24 08:43:50 +0100

    feat(lightbulb): enable again and just ignore razor filetype

--- lua/plugin-management/plugins/nvim-lightbulb.lua
+++ lua/plugin-management/plugins/nvim-lightbulb.lua
@@ -2,14 +2,13 @@
 --- @type LazyPluginSpec
 return {
   "kosayoda/nvim-lightbulb",
-  enabled = not vim.g.use_roslyn,
   opts = {
     autocmd = {
       enabled = true,
     },
     code_lenses = true,
     ignore = {
-      clients = { "null-ls" },
+      ft = { "razor" },
     },
   },
 }
