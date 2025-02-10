ad3e880e5dc8efed6dec6d8134548f9609f11a6e
Kai Moschcau <kai.moschcau@blecon.de>
2025-02-10 08:44:26 +0100

    feat(endwise): switch back to original origin

--- lua/plugin-management/plugins/nvim-treesitter-endwise.lua
+++ lua/plugin-management/plugins/nvim-treesitter-endwise.lua
@@ -1,7 +1,6 @@
 -- selene: allow(mixed_table)
 --- @type LazyPluginSpec
 return {
-  -- "RRethy/nvim-treesitter-endwise",
-  "metiulekm/nvim-treesitter-endwise",
+  "RRethy/nvim-treesitter-endwise",
   dependencies = "nvim-treesitter/nvim-treesitter",
 }
