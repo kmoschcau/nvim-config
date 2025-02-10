5df075eb90332855e2da726fc76702810ee4f5b0
Kai Moschcau <kai.moschcau@blecon.de>
2025-02-10 12:14:50 +0100

    feat(plugins): add nvim-ts-context-commentstring

---
+++ lua/plugin-management/plugins/nvim-ts-context-commentstring.lua
@@ -0,0 +1,18 @@
+-- selene: allow(mixed_table)
+--- @type LazyPluginSpec
+return {
+  "JoosepAlviste/nvim-ts-context-commentstring",
+  enabled = true,
+  config = function()
+    require("ts_context_commentstring").setup {
+      enable_autocmd = false,
+    }
+
+    local get_option = vim.filetype.get_option
+    vim.filetype.get_option = function(filetype, option)
+      return option == "commentstring"
+          and require("ts_context_commentstring.internal").calculate_commentstring()
+        or get_option(filetype, option)
+    end
+  end,
+}
