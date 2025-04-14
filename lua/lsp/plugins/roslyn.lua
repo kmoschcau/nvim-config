0fe94bb75fc964692b9e09f8b0f115c4631afe35
Kai Moschcau <kai.moschcau@blecon.de>
2025-04-14 12:24:52 +0200

    feat(roslyn): remove no longer needed semantic tokens hack

--- lua/lsp/plugins/roslyn.lua
+++ lua/lsp/plugins/roslyn.lua
@@ -1,52 +1,3 @@
---- @param client vim.lsp.Client the LSP client
-local function monkey_patch_semantic_tokens(client)
-  -- NOTE: Super hacky... Don't know if I like that we set a random variable on
-  -- the client Seems to work though ~seblj
-  if client.is_hacked then
-    return
-  end
-  client.is_hacked = true
-
-  -- let the runtime know the server can do semanticTokens/full now
-  client.server_capabilities =
-    vim.tbl_deep_extend("force", client.server_capabilities, {
-      semanticTokensProvider = {
-        full = true,
-      },
-    })
-
-  -- monkey patch the request proxy
-  local request_inner = client.request
-  function client:request(method, params, handler, req_bufnr)
-    if method ~= vim.lsp.protocol.Methods.textDocument_semanticTokens_full then
-      return request_inner(self, method, params, handler)
-    end
-
-    local target_bufnr = vim.uri_to_bufnr(params.textDocument.uri)
-    local line_count = vim.api.nvim_buf_line_count(target_bufnr)
-    local last_line = vim.api.nvim_buf_get_lines(
-      target_bufnr,
-      line_count - 1,
-      line_count,
-      true
-    )[1]
-
-    return request_inner(self, "textDocument/semanticTokens/range", {
-      textDocument = params.textDocument,
-      range = {
-        ["start"] = {
-          line = 0,
-          character = 0,
-        },
-        ["end"] = {
-          line = line_count - 1,
-          character = string.len(last_line) - 1,
-        },
-      },
-    }, handler, req_bufnr)
-  end
-end
-
 local mason_registry = require "mason-registry"
 
 --- @type string[]
@@ -81,11 +32,6 @@ local config = {
   config = {
     capabilities = require("lsp.common").capabilities,
     handlers = require "rzls.roslyn_handlers",
-    on_attach = function(client, bufnr)
-      require "lsp.attach"(client, bufnr)
-
-      monkey_patch_semantic_tokens(client)
-    end,
   },
   filewatching = "off",
 }
