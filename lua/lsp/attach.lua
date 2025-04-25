40d9330c9ec07c4e6889ba41282b7efe02f6745c
Kai Moschcau <kai.moschcau@blecon.de>
2025-04-25 10:37:11 +0200

    refactor(lsp): sort attach handler by method name

--- lua/lsp/attach.lua
+++ lua/lsp/attach.lua
@@ -16,171 +16,6 @@ return function(client, bufnr)
 
   common.log_capabilities(client)
 
-  -- keymaps {{{
-
-  -- textDocument/declaration
-  local declaration_impl, declaration_impl_desc =
-    common.choose_keymap_implementation(
-      keymap_implementation,
-      vim.lsp.buf.declaration,
-      {
-        snacks_impl = common.with_options(
-          snacks.picker.lsp_declarations,
-          snacks_default_options
-        ),
-      }
-    )
-  if declaration_impl then
-    vim.keymap.set("n", "gD", declaration_impl, {
-      buffer = bufnr,
-      desc = "LSP"
-        .. declaration_impl_desc
-        .. ": Go to the declaration of the symbol under the cursor.",
-    })
-  end
-
-  -- textDocument/definition
-  -- (also mapped as limited variant by default as <C-]>, <C-w>] and <C-w>})
-  local definition_impl, definition_impl_desc =
-    common.choose_keymap_implementation(
-      keymap_implementation,
-      vim.lsp.buf.definition,
-      {
-        omni_ext_impl = omni_ext.lsp_definition,
-        snacks_impl = common.with_options(
-          snacks.picker.lsp_definitions,
-          snacks_default_options
-        ),
-      }
-    )
-  if definition_impl then
-    vim.keymap.set("n", "gd", definition_impl, {
-      buffer = bufnr,
-      desc = "LSP("
-        .. definition_impl_desc
-        .. "): Find definitions of the symbol under the cursor.",
-    })
-  end
-
-  -- textDocument/implementation | mapped to gri by default
-  local implementation_impl, implementation_impl_desc =
-    common.choose_keymap_implementation(
-      keymap_implementation,
-      vim.lsp.buf.implementation,
-      {
-        omni_ext_impl = omni_ext.lsp_implementation,
-        snacks_impl = common.with_options(
-          snacks.picker.lsp_implementations,
-          snacks_default_options
-        ),
-      }
-    )
-  if implementation_impl then
-    vim.keymap.set("n", "gri", implementation_impl, {
-      buffer = bufnr,
-      desc = "LSP("
-        .. implementation_impl_desc
-        .. "): Find implementations of the symbol under the cursor.",
-    })
-  end
-
-  -- textDocument/signatureHelp | mapped in insert to <C-s> by default
-  vim.keymap.set({ "n", "i" }, "<C-s>", function()
-    vim.lsp.buf.signature_help()
-  end, {
-    buffer = bufnr,
-    desc = "LSP: Show signature help.",
-  })
-
-  -- workspace/didChangeWorkspaceFolders
-  vim.keymap.set("n", "<Space>wa", vim.lsp.buf.add_workspace_folder, {
-    buffer = bufnr,
-    desc = "LSP: Add a workspace folder.",
-  })
-  vim.keymap.set("n", "<Space>wr", vim.lsp.buf.remove_workspace_folder, {
-    buffer = bufnr,
-    desc = "LSP: Remove a workspace folder.",
-  })
-
-  vim.keymap.set("n", "<Space>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, {
-    buffer = bufnr,
-    desc = "LSP: Print the current workspace folders.",
-  })
-
-  -- textDocument/typeDefinition
-  local type_definition_impl, type_definition_impl_desc =
-    common.choose_keymap_implementation(
-      keymap_implementation,
-      vim.lsp.buf.type_definition,
-      {
-        omni_ext_impl = omni_ext.lsp_type_definition,
-        snacks_impl = common.with_options(
-          snacks.picker.lsp_type_definitions,
-          snacks_default_options
-        ),
-      }
-    )
-  if type_definition_impl then
-    vim.keymap.set("n", "<Space>D", type_definition_impl, {
-      buffer = bufnr,
-      desc = "LSP("
-        .. type_definition_impl_desc
-        .. "): Find type definitions of the symbol under the cursor.",
-    })
-  end
-
-  -- textDocument/rename | mapped to grn by default
-  if client and client.name == "volar" then
-    vim.keymap.set("n", "<Space>grnv", function()
-      vim.lsp.buf.rename(nil, {
-        filter = function(c)
-          -- TODO: Check filetype and injected language instead and move to grn
-          -- keymap.
-          return c.name == "volar"
-        end,
-      })
-    end, {
-      buffer = bufnr,
-      desc = "LSP: Rename the symbol under the cursor with volar",
-    })
-  end
-
-  -- textDocument/codeAction | mapped to gra by default
-
-  -- textDocument/codeLens
-  if common.supports_method(client, "codeLens/resolve") then
-    vim.keymap.set("n", "grc", vim.lsp.codelens.run, {
-      buffer = bufnr,
-      desc = "LSP: Run the code lens in the current line.",
-    })
-  end
-
-  -- textDocument/references | mapped to grr by default
-  local references_impl, references_impl_desc =
-    common.choose_keymap_implementation(
-      keymap_implementation,
-      vim.lsp.buf.references,
-      {
-        omni_ext_impl = omni_ext.lsp_references,
-        snacks_impl = common.with_options(
-          snacks.picker.lsp_references,
-          vim.tbl_deep_extend("force", snacks_default_options, {
-            include_declaration = false,
-          })
-        ),
-      }
-    )
-  if references_impl then
-    vim.keymap.set("n", "grr", references_impl, {
-      buffer = bufnr,
-      desc = "LSP("
-        .. references_impl_desc
-        .. "): Find references for the symbol under the cursor.",
-    })
-  end
-
   -- callHierarchy/incomingCalls
   local incoming_calls_impl, incoming_calls_impl_desc =
     common.choose_keymap_implementation(
@@ -213,6 +48,87 @@ return function(client, bufnr)
     })
   end
 
+  -- textDocument/codeAction
+  -- mapped to gra by default
+  -- see: |vim.lsp.buf.code_action()|
+
+  -- textDocument/codeLens
+  -- see: |lsp-codelens|
+  vim.keymap.set("n", "grc", vim.lsp.codelens.run, {
+    buffer = bufnr,
+    desc = "LSP: Run the code lens in the current line.",
+  })
+  vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave" }, {
+    desc = "LSP: Update the code lenses of the buffer.",
+    group = common.augroup,
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.codelens.refresh { bufnr = bufnr }
+    end,
+  })
+
+  -- textDocument/completion
+  -- plugin: Saghen/blink.cmp
+  -- see: |lsp-completion|
+
+  -- textDocument/declaration
+  local declaration_impl, declaration_impl_desc =
+    common.choose_keymap_implementation(
+      keymap_implementation,
+      vim.lsp.buf.declaration,
+      {
+        snacks_impl = common.with_options(
+          snacks.picker.lsp_declarations,
+          snacks_default_options
+        ),
+      }
+    )
+  if declaration_impl then
+    vim.keymap.set("n", "gD", declaration_impl, {
+      buffer = bufnr,
+      desc = "LSP"
+        .. declaration_impl_desc
+        .. ": Go to the declaration of the symbol under the cursor.",
+    })
+  end
+
+  -- textDocument/definition
+  -- also mapped as limited variant by default as <C-]>, <C-w>] and <C-w>}
+  local definition_impl, definition_impl_desc =
+    common.choose_keymap_implementation(
+      keymap_implementation,
+      vim.lsp.buf.definition,
+      {
+        omni_ext_impl = omni_ext.lsp_definition,
+        snacks_impl = common.with_options(
+          snacks.picker.lsp_definitions,
+          snacks_default_options
+        ),
+      }
+    )
+  if definition_impl then
+    vim.keymap.set("n", "gd", definition_impl, {
+      buffer = bufnr,
+      desc = "LSP("
+        .. definition_impl_desc
+        .. "): Find definitions of the symbol under the cursor.",
+    })
+  end
+
+  -- textDocument/diagnostic
+  -- plugin: mfussenegger/nvim-lint
+  -- see: |lsp-diagnostic|
+  -- see: |vim.diagnostic|
+
+  -- textDocument/documentColor
+  -- see: |lsp-document_color|
+  -- TODO: enable
+
+  -- textDocument/documentHighlight
+  -- plugin: RRethy/vim-illuminate
+  -- see: |vim.lsp.buf.clear_references()|
+  -- see: |vim.lsp.buf.document_highlight()|
+
   -- textDocument/documentSymbol | mapped to gO by default
   local document_symbol_impl, document_symbol_impl_desc =
     common.choose_keymap_implementation(
@@ -227,36 +143,46 @@ return function(client, bufnr)
     })
   end
 
-  -- workspace/symbol
-  local workspace_symbol_impl, workspace_symbol_impl_desc =
+  -- textDocument/foldingRange
+  if common.supports_method(client, "textDocument/foldingRange") then
+    vim.opt_local.foldexpr = "v:lua.vim.lsp.foldexpr()"
+    vim.opt_local.foldmethod = "expr"
+    vim.opt_local.foldtext = "v:lua.vim.lsp.foldtext()"
+  end
+
+  -- textDocument/formatting
+  -- plugin: stevearc/conform.nvim
+  -- see: |vim.lsp.buf.format()|
+
+  -- textDocument/hover
+  -- mapped to K by default
+  -- see: |vim.lsp.buf.hover()|
+
+  -- textDocument/implementation
+  -- mapped to gri by default
+  local implementation_impl, implementation_impl_desc =
     common.choose_keymap_implementation(
       keymap_implementation,
-      vim.lsp.buf.workspace_symbol,
-      { snacks_impl = snacks.picker.lsp_workspace_symbols }
+      vim.lsp.buf.implementation,
+      {
+        omni_ext_impl = omni_ext.lsp_implementation,
+        snacks_impl = common.with_options(
+          snacks.picker.lsp_implementations,
+          snacks_default_options
+        ),
+      }
     )
-  if workspace_symbol_impl then
-    vim.keymap.set("n", "<Space>sw", workspace_symbol_impl, {
+  if implementation_impl then
+    vim.keymap.set("n", "gri", implementation_impl, {
       buffer = bufnr,
       desc = "LSP("
-        .. workspace_symbol_impl_desc
-        .. "): Find workspace symbols.",
+        .. implementation_impl_desc
+        .. "): Find implementations of the symbol under the cursor.",
     })
   end
 
-  -- textDocument/hover | mapped to K by default
-
-  -- textDocument/formatting | see conform plugin
-
-  -- textDocument/rangeFormatting | see conform plugin
-
-  -- textDocument/semanticTokens/full
-  vim.keymap.set("n", "<F9>", vim.lsp.semantic_tokens.force_refresh, {
-    buffer = bufnr,
-    desc = "LSP: Do a full semantic tokens refresh.",
-  })
-
-  -- inlayHint/resolve
   -- textDocument/inlayHint
+  -- see: |lsp-inlay_hint|
   vim.keymap.set("n", "[ih", function()
     vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
   end, {
@@ -279,29 +205,110 @@ return function(client, bufnr)
     desc = "LSP: Toggle inlay hints in the buffer.",
   })
 
-  -- }}}
+  -- textDocument/prepareTypeHierarchy
+  -- see: |vim.lsp.buf.typehierarchy()|
+  -- TODO: add keymaps
 
-  -- settings {{{
+  -- textDocument/publishDiagnostics
+  -- see: textDocument/diagnostic
 
-  if common.supports_method(client, "textDocument/foldingRange") then
-    vim.opt_local.foldexpr = "v:lua.vim.lsp.foldexpr()"
-    vim.opt_local.foldtext = "v:lua.vim.lsp.foldtext()"
-  end
+  -- textDocument/rangeFormatting
+  -- see: textDocument/formatting
 
-  -- }}}
+  -- textDocument/rangesFormatting
+  -- see: textDocument/formatting
 
-  -- autocommands {{{
-
-  if common.supports_method(client, "textDocument/codeLens") then
-    vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave" }, {
-      desc = "LSP: Update the code lenses of the buffer.",
-      group = common.augroup,
+  -- textDocument/references | mapped to grr by default
+  local references_impl, references_impl_desc =
+    common.choose_keymap_implementation(
+      keymap_implementation,
+      vim.lsp.buf.references,
+      {
+        omni_ext_impl = omni_ext.lsp_references,
+        snacks_impl = common.with_options(
+          snacks.picker.lsp_references,
+          vim.tbl_deep_extend("force", snacks_default_options, {
+            include_declaration = false,
+          })
+        ),
+      }
+    )
+  if references_impl then
+    vim.keymap.set("n", "grr", references_impl, {
       buffer = bufnr,
-      callback = function()
-        vim.lsp.codelens.refresh { bufnr = bufnr }
-      end,
+      desc = "LSP("
+        .. references_impl_desc
+        .. "): Find references for the symbol under the cursor.",
     })
   end
 
-  -- }}}
+  -- textDocument/rename
+  -- mapped to grn by default
+  if client and client.name == "volar" then
+    vim.keymap.set("n", "<Space>grnv", function()
+      vim.lsp.buf.rename(nil, {
+        filter = function(c)
+          -- TODO: Check filetype and injected language instead and move to grn
+          -- keymap.
+          return c.name == "volar"
+        end,
+      })
+    end, {
+      buffer = bufnr,
+      desc = "LSP: Rename the symbol under the cursor with volar",
+    })
+  end
+
+  -- textDocument/semanticTokens/full
+  -- see: |lsp-semantic_tokens|
+
+  -- textDocument/semanticTokens/full/delta
+  -- see: textDocument/semanticTokens/full
+
+  -- textDocument/signatureHelp
+  -- mapped in insert to <C-s> by default
+  -- TODO: Add custom handler for overloads.
+  -- https://github.com/neovim/neovim/blob/mater/runtime/lua/vim/lsp/handlers.lua#L411
+  -- https://github.com/neovim/neovim/blob/master/runtime/lua/vim/lsp/util.lua#L720
+  vim.keymap.set({ "n", "i" }, "<C-s>", vim.lsp.buf.signature_help, {
+    buffer = bufnr,
+    desc = "LSP: Show signature help.",
+  })
+
+  -- textDocument/typeDefinition
+  local type_definition_impl, type_definition_impl_desc =
+    common.choose_keymap_implementation(
+      keymap_implementation,
+      vim.lsp.buf.type_definition,
+      {
+        omni_ext_impl = omni_ext.lsp_type_definition,
+        snacks_impl = common.with_options(
+          snacks.picker.lsp_type_definitions,
+          snacks_default_options
+        ),
+      }
+    )
+  if type_definition_impl then
+    vim.keymap.set("n", "<Space>D", type_definition_impl, {
+      buffer = bufnr,
+      desc = "LSP("
+        .. type_definition_impl_desc
+        .. "): Find type definitions of the symbol under the cursor.",
+    })
+  end
+
+  -- typeHierarchy/subtypes
+  -- see: textDocument/prepareTypeHierarchy
+
+  -- typeHierarchy/supertypes
+  -- see: textDocument/prepareTypeHierarchy
+
+  -- workspace/symbol
+  -- plugin: folke/snacks.nvim
+
+  -- workspace/workspaceFolders
+  -- not used
+  -- see: |vim.lsp.buf.add_workspace_folder()|
+  -- see: |vim.lsp.buf.list_workspace_folders()|
+  -- see: |vim.lsp.buf.remove_workspace_folder()|
 end
