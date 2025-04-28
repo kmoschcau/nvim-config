abd6b04d57745dbcc516dab31c78df1df0ed2621
Kai Moschcau <kai.moschcau@blecon.de>
2025-04-28 10:18:58 +0200

    feat(terminal): add OSC 133 TermRequest handlers

--- lua/terminal.lua
+++ lua/terminal.lua
@@ -9,3 +9,87 @@ vim.api.nvim_create_autocmd("TermOpen", {
     vim.opt_local.spell = false
   end,
 })
+
+-- TODO: Clear these namespaces on refresh. Maybe clear them, when a new
+-- TermRequest is received for a line higher up than the last line.
+
+local term_prompt_ns = vim.api.nvim_create_namespace "TermPromptMarkers"
+local term_error_ns = vim.api.nvim_create_namespace "TermErrorDiagnostics"
+
+vim.api.nvim_create_autocmd("TermRequest", {
+  desc = "Add extmarks for OSC 133;A requests.",
+  group = augroup,
+  ---@param args TermRequestArgs
+  callback = function(args)
+    if not string.match(args.data.sequence, "^\027]133;A") then
+      return
+    end
+
+    local row = args.data.cursor[1] - 1
+    local col = 0
+    local row_col = { row, col }
+
+    local existing_marks = vim.api.nvim_buf_get_extmarks(
+      args.buf,
+      term_prompt_ns,
+      row_col,
+      row_col,
+      {}
+    )
+    for _, existing_mark in ipairs(existing_marks) do
+      vim.api.nvim_buf_del_extmark(args.buf, term_prompt_ns, existing_mark[1])
+    end
+
+    vim.api.nvim_buf_set_extmark(args.buf, term_prompt_ns, row, col, {
+      sign_hl_group = "@punctuation.special",
+      sign_text = require("symbols").signs.term_prompt,
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd("TermRequest", {
+  desc = "Add diagnostics for OSC 133;D requests with error exit codes.",
+  group = augroup,
+  ---@param args TermRequestArgs
+  callback = function(args)
+    if not string.match(args.data.sequence, "^\027]133;D") then
+      return
+    end
+
+    -- See: https://gitlab.freedesktop.org/Per_Bothner/specifications/blob/master/proposals/semantic-prompts.md#commands
+
+    ---@type string?
+    local exit_code = string.match(args.data.sequence, "^\027]133;D;(%d+)")
+
+    ---@type string?
+    local err_message =
+      string.match(args.data.sequence, "^\027]133;D;%d+;err=(.+)")
+
+    if
+      (not exit_code or exit_code == "0")
+      and (not err_message or err_message == "")
+    then
+      return
+    end
+
+    local row = args.data.cursor[1] - 1
+    local col = args.data.cursor[2]
+
+    local diagnostics =
+      vim.diagnostic.get(args.buf, { namespace = term_error_ns })
+
+    ---@type vim.Diagnostic
+    local diagnostic = {
+      lnum = row,
+      col = col,
+      end_col = col,
+      severity = vim.diagnostic.severity.ERROR,
+      message = err_message or exit_code or "",
+      source = "exit-code",
+      code = exit_code,
+    }
+    table.insert(diagnostics, diagnostic)
+
+    vim.diagnostic.set(term_error_ns, args.buf, diagnostics)
+  end,
+})
