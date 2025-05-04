c6195686539c83f119688776efe1e1cc96aea55a
Kai Moschcau <kai@kmoschcau.de>
2025-05-04 14:15:57 +0200

    fix(lsp): move all LSP configs to the after dir

--- after/lsp/roslyn_ls.lua
+++ after/lsp/roslyn_ls.lua
@@ -1,6 +1,5 @@
 ---@type vim.lsp.Config
 return {
-  -- FIXME: This isn't overriding the default for some reason.
   cmd = require("lsp.helpers").get_roslyn_cmd(),
   root_dir = function(bufnr, cb)
     local config =
