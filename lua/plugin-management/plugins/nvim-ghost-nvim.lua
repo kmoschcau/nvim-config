73c416f8f3c6a413aec30eb753109e03610300e6
Kai Moschcau <kai.moschcau@blecon.de>
2025-05-22 09:10:13 +0200

    feat(nvim-ghost): set up filetype for github

--- lua/plugin-management/plugins/nvim-ghost-nvim.lua
+++ lua/plugin-management/plugins/nvim-ghost-nvim.lua
@@ -2,4 +2,17 @@
 ---@type LazyPluginSpec
 return {
   "subnut/nvim-ghost.nvim",
+  init = function()
+    local augroup =
+      vim.api.nvim_create_augroup("nvim_ghost_user_autocommands", {})
+
+    vim.api.nvim_create_autocmd("User", {
+      desc = "nvim-ghost: Set markdown filetype",
+      group = augroup,
+      pattern = "*github.com",
+      callback = function()
+        vim.opt.filetype = "markdown"
+      end,
+    })
+  end,
 }
