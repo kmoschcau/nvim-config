6b8161af220d42ff0147f2fc73217afe6031e167
Kai Moschcau <kai.moschcau@blecon.de>
2025-05-23 14:47:24 +0200

    feat(nvim-ghost): add dev.azure.com

--- lua/plugin-management/plugins/nvim-ghost-nvim.lua
+++ lua/plugin-management/plugins/nvim-ghost-nvim.lua
@@ -3,13 +3,18 @@
 return {
   "subnut/nvim-ghost.nvim",
   init = function()
+    local patterns = {
+      "*github.com",
+      "dev.azure.com",
+    }
+
     local augroup =
       vim.api.nvim_create_augroup("nvim_ghost_user_autocommands", {})
 
     vim.api.nvim_create_autocmd("User", {
       desc = "nvim-ghost: Set markdown filetype",
       group = augroup,
-      pattern = "*github.com",
+      pattern = table.concat(patterns, ","),
       callback = function()
         vim.opt.filetype = "markdown"
       end,
