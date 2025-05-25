53499a97ad5c9336c5541e67a76e763f2701af00
Kai Moschcau <kai.moschcau@blecon.de>
2025-05-25 11:32:23 +0200

    docs(ftplugin): remove old formatting comments

--- after/ftplugin/ps1.lua
+++ after/ftplugin/ps1.lua
@@ -1,8 +1,2 @@
--- vim: foldmethod=marker
--- Powershell file type settings
-
--- general Neovim settings {{{1
--- Neovim options {{{2
-
 vim.opt_local.formatoptions = nil
 vim.opt_local.textwidth = 80
