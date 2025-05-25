ad152533ebd000e5e763eaed2f6e1420ed39fa2f
Kai Moschcau <kai.moschcau@blecon.de>
2025-05-25 14:13:16 +0200

    chore(plugins): update

--- lua/plugin-management/plugins/nvim-treesitter.lua
+++ lua/plugin-management/plugins/nvim-treesitter.lua
@@ -2,9 +2,9 @@
 ---@type LazyPluginSpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = function()
-    require("nvim-treesitter.install").update {}()
-  end,
+  lazy = false,
+  branch = "master",
+  build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup {
       ensure_installed = {
