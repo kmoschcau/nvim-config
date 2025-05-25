d03378efd15386e14a93b28713696dc1328e55d4
Kai Moschcau <kai.moschcau@blecon.de>
2025-05-25 13:11:12 +0200

    feat(nvim-ghost): unset textwidth for GitHub
    This is done because GitHub's markdown parser introduces hard line
    breaks in the output for newlines in the input.
    
    Markdown should not behave like that. 😤

--- lua/plugin-management/plugins/nvim-ghost-nvim.lua
+++ lua/plugin-management/plugins/nvim-ghost-nvim.lua
@@ -8,6 +8,10 @@ return {
       "dev.azure.com",
     }
 
+    local hard_break_sites_patterns = {
+      "*github.com",
+    }
+
     local augroup =
       vim.api.nvim_create_augroup("nvim_ghost_user_autocommands", {})
 
@@ -17,6 +21,15 @@ return {
       pattern = table.concat(patterns, ","),
       callback = function()
         vim.opt.filetype = "markdown"
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      desc = "nvim-ghost: Set textwidth to 0 for sites that break lines in the output on line breaks",
+      group = augroup,
+      pattern = table.concat(hard_break_sites_patterns, ","),
+      callback = function()
+        vim.opt_local.textwidth = 0
       end,
     })
   end,
