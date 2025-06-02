03a2affd075121c185e5ac9933737ffc9ad2ebbc
Kai Moschcau <kai.moschcau@blecon.de>
2025-06-02 13:12:41 +0200

    feat(matchup): disable off-screen popup, as it is currently broken
    See: https://github.com/andymass/vim-matchup/issues/391

--- lua/plugin-management/plugins/vim-matchup.lua
+++ lua/plugin-management/plugins/vim-matchup.lua
@@ -5,6 +5,11 @@ return {
   dependencies = "nvim-treesitter/nvim-treesitter",
   event = "BufReadPost",
   init = function()
-    vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    -- NOTE: This is currently broken.
+    -- See: https://github.com/andymass/vim-matchup/issues/391
+    -- vim.g.matchup_matchparen_offscreen = {
+    --   border = "none",
+    --   method = "popup",
+    -- }
   end,
 }
