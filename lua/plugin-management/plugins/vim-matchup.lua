a15859c89b4d95bddcd393bda814a0fef89feae6
Kai Moschcau <kai.moschcau@blecon.de>
2025-06-05 07:51:02 +0200

    fix(vim-matchup): update and reenable off-screen matchparen

--- lua/plugin-management/plugins/vim-matchup.lua
+++ lua/plugin-management/plugins/vim-matchup.lua
@@ -5,11 +5,9 @@ return {
   dependencies = "nvim-treesitter/nvim-treesitter",
   event = "BufReadPost",
   init = function()
-    -- NOTE: This is currently broken.
-    -- See: https://github.com/andymass/vim-matchup/issues/391
-    -- vim.g.matchup_matchparen_offscreen = {
-    --   border = "none",
-    --   method = "popup",
-    -- }
+    vim.g.matchup_matchparen_offscreen = {
+      border = "none",
+      method = "popup",
+    }
   end,
 }
