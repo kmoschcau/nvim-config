a2b0c44d2e887add229c782959466f883c99d495
Kai Moschcau <kai@kmoschcau.de>
2025-06-20 21:46:07 +0200

    feat(lazy): add a user command to build plugins after first install

--- lua/plugin-management/lazy-config.lua
+++ lua/plugin-management/lazy-config.lua
@@ -15,3 +15,12 @@ require("lazy").setup("plugin-management.plugins", {
     browser = require("system-compat").get_browser_command(),
   },
 })
+
+-- TODO: Remove once I have an alternative to markdown-preview.nvim
+vim.api.nvim_create_user_command("LazyBuild", function()
+  vim.cmd [[Lazy load markdown-preview.nvim]]
+  vim.cmd [[Lazy build all]]
+end, {
+  bar = true,
+  desc = "Lazy: Build all plugins that need building.",
+})
