356f4fb5c041286879cee612b25f69ddf2d8b09a
Kai Moschcau <kai@kmoschcau.de>
2025-06-20 20:14:59 +0200

    feat: add feedback messages to lazy bootstrap

--- lua/plugin-management/lazy-bootstrap.lua
+++ lua/plugin-management/lazy-bootstrap.lua
@@ -1,14 +1,25 @@
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fs.joinpath(vim.fn.stdpath "data", "lazy", "lazy.nvim")
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim
+  vim.api.nvim_echo({ { "Cloning lazy.nvim…\n" } }, true, { kind = "echo" })
+  local result = vim
     .system({
       "git",
       "clone",
       "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
       "--branch=stable",
+      "https://github.com/folke/lazy.nvim.git",
       lazypath,
-    })
+    }, { text = true })
     :wait()
+
+  if result.code ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { result.stderr, "ErrorMsg" },
+      { result.stdout, "WarningMsg" },
+    }, true, { kind = "emsg" })
+  end
 end
+
 vim.opt.rtp:prepend(lazypath)
