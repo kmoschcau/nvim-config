03e0af106f3e458b7cc2ef02478f902ccec60a26
Kai Moschcau <kai.moschcau@blecon.de>
2025-08-08 09:24:13 +0200

    fix(lazy): remove backdrop border
    Workaround until https://github.com/folke/lazy.nvim/issues/1951 is
    solved.

--- lua/plugin-management/lazy-config.lua
+++ lua/plugin-management/lazy-config.lua
@@ -20,6 +20,17 @@ require("lazy").setup("plugin-management.plugins", {
   },
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Lazy.nvim: Remove the border from the backdrop window",
+  pattern = "lazy_backdrop",
+  group = vim.api.nvim_create_augroup("lazy.nvim-winborder", { clear = true }),
+  callback = function(args)
+    for _, window_id in ipairs(vim.fn.win_findbuf(args.buf)) do
+      vim.api.nvim_win_set_config(window_id, { border = "none" })
+    end
+  end,
+})
+
 -- TODO: Remove once I have an alternative to markdown-preview.nvim
 vim.api.nvim_create_user_command("LazyBuild", function()
   vim.cmd [[Lazy load markdown-preview.nvim]]
