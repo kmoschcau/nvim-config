c72682394e79d569cbb790e0fb2998c663ec089f
Kai Moschcau <kai.moschcau@blecon.de>
2025-09-02 08:46:57 +0200

    feat(vue): set 'comments' to javascript value
    This is far more useful than the HTML-template default value.

---
+++ after/ftplugin/vue.lua
@@ -0,0 +1,4 @@
+-- For some reason vim.filetype.get_option gives a different result here, so
+-- just set this explicitly.
+vim.opt_local.comments =
+  { "sO:* -", "mO:*  ", "exO:*/", "s1:/*", "mb:*", "ex:*/", "://" }
