e18ac2d29876e964163cf0ef00689279794f65a6
Kai Moschcau <kai.moschcau@blecon.de>
2025-09-16 08:35:10 +0200

    feat(plugins): add wezterm-types for lazydev

--- lua/plugin-management/plugins/lazydev-nvim.lua
+++ lua/plugin-management/plugins/lazydev-nvim.lua
@@ -7,12 +7,16 @@ return {
   -- cspell:disable-next-line
   "folke/lazydev.nvim",
   ft = "lua",
+  dependencies = {
+    { "DrKJeff16/wezterm-types", lazy = true },
+  },
   ---@module "lazydev"
   ---@type lazydev.Config
   opts = {
     ---@type lazydev.Library.spec[]
     library = {
       "luvit-meta/library",
+      { path = "wezterm-types", mods = { "wezterm" } },
     },
   },
 }
