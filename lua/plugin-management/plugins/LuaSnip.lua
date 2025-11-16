315f2166bc5a7dd9ee9fb332c55547e5e49c5bf2
Kai Moschcau <kai@kmoschcau.de>
2025-11-16 11:59:07 +0100

    refactor: move snippets into kmo

--- lua/plugin-management/plugins/LuaSnip.lua
+++ lua/plugin-management/plugins/LuaSnip.lua
@@ -15,7 +15,7 @@ return {
     require("luasnip.loaders.from_vscode").lazy_load()
 
     for _, ft_path in
-      ipairs(vim.api.nvim_get_runtime_file("lua/snippets/*.lua", true))
+      ipairs(vim.api.nvim_get_runtime_file("lua/kmo/snippets/*.lua", true))
     do
       loadfile(ft_path)()
     end
