322249841874179e1229d855324c433e074f9225
Kai Moschcau <kai@kmoschcau.de>
2025-11-16 12:08:04 +0100

    refactor: move extui into kmo

--- init.lua
+++ init.lua
@@ -30,7 +30,7 @@ xpcall(require, error_handler, "kmo.lsp")
 -- configuration modules {{{
 
 xpcall(require, error_handler, "kmo.diagnostics")
-xpcall(require, error_handler, "extui")
+xpcall(require, error_handler, "kmo.extui")
 xpcall(require, error_handler, "filetype")
 xpcall(require, error_handler, "kmo.treesitter")
 xpcall(require, error_handler, "terminal")
