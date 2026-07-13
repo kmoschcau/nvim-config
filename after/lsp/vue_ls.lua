d443983645193373f06b2af917d4bc0a7d253ee8
Kai Moschcau <kai.moschcau@blecon.de>
2026-07-13 10:19:22 +0200

    fix(lsp): vue-language-server crashing on startup

--- after/lsp/vue_ls.lua
+++ after/lsp/vue_ls.lua
@@ -1,5 +1,14 @@
 ---@type vim.lsp.Config
 return {
+  -- HACK: This is only needed for as long as the following issue is not
+  -- resolved: https://github.com/mason-org/mason-registry/issues/15934
+  cmd = {
+    "vue-language-server",
+    "--stdio",
+    "--tsdk="
+      .. vim.fn.expand "$MASON/packages/vtsls/node_modules/@vtsls/language-server/node_modules/typescript/lib",
+  },
+
   -- There seems to be no official documentation of this. Just check in
   -- VSCodium.
   settings = {
