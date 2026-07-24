180e186679610209767b0f2a82885790e54a2d45
Kai Moschcau <kai.moschcau@blecon.de>
2026-07-24 14:20:19 +0200

    feat(snippets): add modern for-in loop for ecma

--- lua/kmo/snippets/ecmascript.lua
+++ lua/kmo/snippets/ecmascript.lua
@@ -10,16 +10,43 @@ local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 
+local for_in = s(
+  {
+    name = "Modern for-in loop",
+    trig = "mforin", -- cspell:disable-line
+    desc = "A modern version of the existing for-in loop snippet",
+  },
+  fmt(
+    [[
+      for (const {key} in {object}) {{
+        if (Object.hasOwn({object_rep}, {key_rep})) {{
+          const {element} = {object_rep}[{key_rep}];
+          {body}
+        }}
+      }}
+    ]],
+    {
+      key = i(1, "key"),
+      key_rep = rep(1),
+      object = i(2, "object"),
+      object_rep = rep(2),
+      element = i(3, "element"),
+      body = i(0),
+    }
+  )
+)
+
 local is_not_instanceof = s(
   {
     name = "Not instanceof",
     trig = "ninst", -- cspell:disable-line
-    desc = "A check to throw an error, when a variable is not an instance of a type.",
+    desc = "A check to throw an error, when a variable is not an instance of a type",
   },
   fmt(
     [[
-      if (!({variable} instanceof {type}))
+      if (!({variable} instanceof {type})) {{
         throw new Error("'{variable_rep}' was not an instance of '{type_rep}'.");
+      }}
     ]],
     {
       variable = i(1),
@@ -38,6 +65,7 @@ local langs =
 
 for _, lang in ipairs(langs) do
   ls.add_snippets(lang, {
+    for_in,
     is_not_instanceof,
   })
 end
